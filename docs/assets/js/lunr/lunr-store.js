e05ceabf9da99a922f6312103896dc20a39dcc23
learn-software-engineering <learn.software.eng@gmail.com>
2022-10-30 22:30:33 +0000

    Publish revision 2b01276210aa6a54b9e2f8164663b926ae62df27

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Post: Modified Date",
+        "excerpt":"This post has been updated and should show a modified date if used in a layout.   First paragraph to test.   Second paragraph.   Third paragraph.   Fourth paragraph.   Fifth paragraph.  ","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/staging-environment/blog/post-modified/",
+        "teaser": null
+      },{
+        "title": "Post: Standard with long title to test overlay image",
+        "excerpt":"First paragraph to test.     This post has a manual excerpt &lt;!--more--&gt; set after the second paragraph. The following YAML Front Matter has also be applied:   excerpt_separator: \"&lt;!--more--&gt;\"   Section 1   Second paragraph.   Section 1.1   Third paragraph.   Section 1.1.1   Fourth paragraph.   Section 1.1.2   Fifth paragraph.   Section 1.2   Sixth paragraph.   Section 2   Seventh paragraph.   Section 2.1   Eighth paragraph.   Section 2.2   Ninth paragraph.   Section 2.2.1   Tenth paragraph.   Section 2.2.2   Eleventh paragraph.  ","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/staging-environment/blog/post-standard/",
+        "teaser": null
+      },{
+        "title": "Post: future date",
+        "excerpt":"Test future post in draft folder.  ","categories": ["Future","Draft"],
+        "tags": ["future","draft","test"],
+        "url": "/staging-environment/future/draft/future-post-template-for-new-posts/",
+        "teaser": null
+      }]
